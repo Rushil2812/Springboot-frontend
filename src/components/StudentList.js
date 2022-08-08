3532bb4349012f114884e1a836158dc88db14cac
rushil2812 <20it164@charusat.edu.in>
2022-08-08 13:47:37 -0700

    First

---
+++ src/components/StudentList.js
@@ -0,0 +1,45 @@
+import { React, useState, useEffect } from "react";
+import { Button, Card, Container, Table } from "react-bootstrap";
+import axios from "axios";
+export default function StudentList() {
+  const [student, setStudent] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8080/listStudents")
+      .then(response => setStudent(response.data))
+      .catch(error => alert(error))
+  })
+  let deleterecord = ()=>{
+    alert("Deleted")
+  }
+  return (
+    <div className="my-3">
+      <Container>
+        <Card.Header><h3>Students List</h3></Card.Header>
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Student Id</th>
+                <th>Student Name</th>
+                <th>Student Address</th>
+                <th>Edit/Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {student.map(student => (
+                <tr>
+                  <td>{student.id}</td>
+                  <td>{student.name}</td>
+                  <td>{student.address}</td>
+                  <td><Button variant="info">Edit</Button>{' '}<Button variant="info" onClick={deleterecord}>Delete</Button></td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Container>
+    </div>
+  );
+}
+
+{/*  */ }
